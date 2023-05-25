15e9e6c18c71721e629194b962bb07f1e3d35d5c
@Seradox24 <ma.inayao@alumnos.duoc.cl>
2023-05-25 05:16:59 -0400

    fix

--- Tienda/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Usuario, Vehiculo, Repuesto
-
-admin.site.register(Usuario)
-admin.site.register(Vehiculo)
-admin.site.register(Repuesto)
--- Tienda/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def home_inicio(request):
-    return render(request,'home/home.html', {} )
-
-def prueba(request):
-    return render(request,'prueba.html')
